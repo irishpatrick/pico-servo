93d81b5d8a5a50f3df14dfaca9aa1eb66150892d
Patrick Roche <rochep63@gmail.com>
2021-02-06 18:10:16 -0500

    problems w/ 2 servos

--- examples/simple.c
+++ examples/simple.c
@@ -7,13 +7,15 @@ int main()
 
     servo_init();
     servo_attach(2);
-    servo_move_to(2, 250);
+    servo_attach(3);
 
     while (1)
     {
         servo_move_to(2, 0);
+        servo_move_to(15, 0);
         sleep_ms(1000);
         servo_move_to(2, 180);
+        servo_move_to(15, 90);
         sleep_ms(1000);
     }
     return 0;
