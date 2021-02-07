23670d47b1183862e98eafb2f74d8bf90120d178
Patrick Roche <rochep63@gmail.com>
2021-02-07 14:47:35 -0500

    license

--- examples/simple.c
+++ examples/simple.c
@@ -1,3 +1,26 @@
+// MIT License
+
+// Copyright (c) 2021 Patrick Roche
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
 #include "pico/stdlib.h"
 #include "pico_servo.h"
 #include <stdio.h>
@@ -11,7 +34,7 @@ int main()
 
     servo_init();
     servo_clock_auto();
-    
+
     servo_attach(A);
     servo_attach(B);
 
@@ -19,10 +42,10 @@ int main()
     {
         servo_move_to(A, 0);
         servo_move_to(B, 0);
-        sleep_ms(1000);
+        sleep_ms(500);
         servo_move_to(A, 180);
         servo_move_to(B, 90);
-        sleep_ms(1000);
+        sleep_ms(500);
     }
     return 0;
 }
